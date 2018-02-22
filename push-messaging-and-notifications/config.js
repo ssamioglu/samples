03433ee2088add854465f6697ff344232437827b
ssamioglu <36730993+ssamioglu@users.noreply.github.com>
2018-02-22 14:11:50 +0300

    Update and rename config.sample.js to config.js

---
+++ push-messaging-and-notifications/config.js
@@ -0,0 +1,4 @@
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+  gcmAPIKey: 'AAAA3lH3Pw4:APA91bGOCy-NDAGizQOZnWPAkIxuse_JJp4XXoEIEeDXuQU2pHrbLFAEthU0ODUFAUyvTQKvqOcR5n2cRsvDYZZrdGZ8GhzfANRbMbNFJ9IJooqTVjML3wdVgzBj_bJFW3Yzf_FRDi_c'
+};
